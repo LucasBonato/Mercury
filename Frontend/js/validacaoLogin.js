44553770e54e5b96e20b85a3277744eb10b9a9b6
Natã <nata_oliveira1605@hotmail.com>
2023-11-30 04:10:23 -0300

    TCM - Mercury 30-11-23

--- Frontend/js/validacaoLogin.js
+++ Frontend/js/validacaoLogin.js
@@ -36,7 +36,7 @@ senha.addEventListener("change", () =>{
 
 
 
-// Mensagem de erro ao efetuar Login
+// Mensagem de erro ao efetuar Login MOBILE / TABLET
 let login_facebook = document.getElementById('btn_facebook'),
 login_google = document.getElementById('btn_google'),
 login_apple = document.getElementById('btn_apple'),
@@ -59,7 +59,35 @@ login_entrar.addEventListener("click", () =>{
     msgError();    
 });
 
+// Mensagem de erro ao efetuar Login DESKTOP
+let login_facebook_desk = document.getElementById('btn_facebook-desk'),
+login_google_desk = document.getElementById('btn_google-desk'),
+login_apple_desk = document.getElementById('btn_apple-desk'),
+esqueciSenha_desk = document.getElementById('esqueci-desk'),
+login_entrar_desk = document.getElementById('btn_entrar-desk');
+
+login_facebook_desk.addEventListener("click", () =>{
+    msgError();
+});
+login_google_desk.addEventListener("click", () =>{
+    msgError();
+});
+login_apple_desk.addEventListener("click", () =>{
+    msgError();    
+});
+esqueciSenha_desk.addEventListener("click", () =>{
+    msgError();    
+});
+login_entrar_desk.addEventListener("click", () =>{
+    msgError();    
+});
 
 function msgError(){
     window.alert("Nosso sistema de login ainda está em desenvolvimento!\n" + "Agradeçemos a compreensão ;)"); 
 }
+
+function msgLogin(){
+    window.alert("Atenção! Para poder comprar é necessário estar logado."); 
+}
+
+window.onload = msgLogin;
